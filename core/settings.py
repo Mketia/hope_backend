73ed84ccfecedbaaef2570e8d13c0410f58f1fe2
Mketia <k.mukakalis1@alustudent.com>
2025-04-03 13:26:11 +0200

    CORS update

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'hope-backend-9gc3.onrender.com']
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173", "http://127.0.0.1:8000", "https://hope-frontend-v3v5.vercel.app" ,"https://hope-frontend-kappa.vercel.app","http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173", "http://127.0.0.1:8000", "https://hope-frontend-v3v5.vercel.app" ,"https://hope-frontend-kappa.vercel.app","http://localhost:5173","https://hope-frontend-six.vercel.app"]
 
 
 # Application definition
